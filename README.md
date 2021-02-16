834af012ed38b60f105a400ab2f4b9f60cb7525b
Alexander Bakhtin <grandiozius@gmail.com>
2021-02-16 20:45:50 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Test Package
+
+My test package on [packagist.org](https://packagist.org/)
+
+Example in examples/example.php
---
+++ src/Test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace grandiozius\TestPackage;
+
+class Test
+{
+    public static function test()
+    {
+        echo 'test' . PHP_EOL;
+    }
+}
